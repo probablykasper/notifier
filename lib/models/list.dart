ebf681eb537af73ff930f0228f9fea2e55fc5492
Kasper <kasperkh.kh@gmail.com>
2019-05-06 17:00:05 +0200

    Model for the list of notifications

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -1,5 +1,15 @@
+import 'dart:collection';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:notifier/models/src/notification.dart';
 
 class ListModel extends Model {
-  
+  final List<Notification> _notifications = [];
+
+  UnmodifiableListView<Notification> get items => UnmodifiableListView(_notifications);
+
+  void add(Notification notification) {
+    _notifications.add(notification);
+    notifyListeners(); // tell the model to rebuild the widgets that depend on it
+  }
+
 }
---
+++ lib/models/src/notification.dart
@@ -0,0 +1,7 @@
+class Notification {
+  // final int id;
+  final String title;
+  final String description;
+
+  Notification({this.title, this.description});
+}
