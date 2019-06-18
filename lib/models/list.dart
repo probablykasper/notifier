72b579161ac5eea497977a09c5f1a55478d04b3c
Kasper <kasperkh.kh@gmail.com>
2019-06-18 22:17:00 +0200

    added background_fetch plugin
    Default notification date is now the next day

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -6,6 +6,13 @@ import 'dart:ui';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:background_fetch/background_fetch.dart';
+
+// This "Headless Task" is run when app is terminated.
+void backgroundFetchHeadlessTask() async {
+  print('[BackgroundFetch] Headless event received.');
+  BackgroundFetch.finish();
+}
 
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
@@ -43,8 +50,58 @@ class ListModel extends Model {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String notificationItems = prefs.getString('notificationItems') ?? '{}';
     _notificationItems = json.decode(notificationItems);
-    print('ListModel _load');
+
+    // Register to receive BackgroundFetch events after app is terminated.
+    // Requires {stopOnTerminate: false, enableHeadless: true}
+    // BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+    configureBackgroundFetch();
+
+    print('[notifier] ListModel _load');
     notifyListeners();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> configureBackgroundFetch() async {
+    // Configure BackgroundFetch.
+    BackgroundFetch.configure(
+        BackgroundFetchConfig(
+          minimumFetchInterval: 15,
+          stopOnTerminate: false,
+          enableHeadless: true,
+          startOnBoot: true,
+        ), () async {
+      // This is the fetch-event callback.
+      print('[BackgroundFetch] Event received');
+      // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+      // for taking too long in the background.
+      await _setNotifications();
+      BackgroundFetch.finish();
+    }).then((int status) {
+      print('[BackgroundFetch] SUCCESS: $status');
+    }).catchError((e) {
+      print('[BackgroundFetch] ERROR: $e');
+    });
+  }
+
+  _setNotifications() async {
+    print('[notifier] _setNotification');
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'scheduled_notifications',
+      'Scheduled Notifications',
+      'Scheduled notifications configured by the user',
+      icon: 'app_icon',
+      color: Color(0x00B0FF),
+      ledColor: Color(0x00B0FF),
+      ledOnMs: 1000,
+      ledOffMs: 500,
+      enableLights: true,
+    );
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      androidPlatformChannelSpecifics,
+      iOSPlatformChannelSpecifics,
+    );
   }
 
   _save() async {
@@ -53,46 +110,13 @@ class ListModel extends Model {
     print('ListModel _save');
   }
 
-  Future<void> _addNotification(dynamic notificationItem) async {
-    print('_addNotification');
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'your other channel id',
-        'your other channel name',
-        'your other channel description',
-        icon: 'app_icon',
-        color: Color(0x00B0FF),
-        ledColor: Color(0x00B0FF),
-        ledOnMs: 1000,
-        ledOffMs: 500,
-        enableLights: true,
-        );
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-      androidPlatformChannelSpecifics,
-      iOSPlatformChannelSpecifics,
-    );
-    if (notificationItem['repeat'] == 'never') {
-      await flutterLocalNotificationsPlugin.schedule(
-        int.parse(notificationItem['id']),
-        notificationItem['title'],
-        notificationItem['description'],
-        DateTime.fromMillisecondsSinceEpoch(notificationItem['date']),
-        platformChannelSpecifics,
-      );
-      print('SCHEDULED NOTIFICATION');
-    } else if (notificationItem['repeat'] == 'daily') {
-    } else if (notificationItem['repeat'] == 'weekly') {
-    } else if (notificationItem['repeat'] == 'monthly') {
-    } else if (notificationItem['repeat'] == 'yearly') {}
-  }
-
   void add(dynamic notificationItem) async {
     final int idLen = 9;
     final String id = Random.secure().nextInt(pow(10, idLen)).toString();
     notificationItem['id'] = id;
     _notificationItems[id] = notificationItem;
     await _save();
-    await _addNotification(notificationItem);
+    await _setNotifications();
     print('ListModel add');
 
     notifyListeners();
@@ -109,7 +133,7 @@ class ListModel extends Model {
     notificationItem['id'] = id;
     _notificationItems[id] = notificationItem;
     await _save();
-    await _addNotification(notificationItem);
+    await _setNotifications();
     print('ListModel update');
     notifyListeners();
   }
